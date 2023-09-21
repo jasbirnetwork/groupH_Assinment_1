be19808f8294cb2c00386d54cadc98144181aca0
Jasbir Network <jasbir.network@outlook.com>
2023-09-21 19:44:35 -0400

    Group H Assignment 1 Code for main.go and Mod function

---
+++ README.md
@@ -0,0 +1,1 @@
+# groupH_Assinment_1
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func Mod(num1 float64, num2 float64) float64 {
+	return math.Mod(num1, num2)
+}
+
+func main() {
+	var num1, num2 float64
+	var operator string
+
+	fmt.Print("Enter the first number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Enter an operator (+, -, *, /, %, ^): ")
+	fmt.Scanln(&operator)
+
+	fmt.Print("Enter the second number: ")
+	fmt.Scanln(&num2)
+
+	result := 0.0
+
+	switch operator {
+	case "+":
+		result = addition() // DHIRAJ TODO
+	case "-":
+		result = subtract() // Randeep TODO
+	case "*":
+		result = multiply() // Abhijeet TODO
+	case "/":
+		if num2 != 0 {
+			result = divison() // Zubiyan TODO
+		} else {
+			fmt.Println("Error: Division by zero.")
+			return
+		}
+	case "%": 
+		if num2 != 0 {
+			result = Mod(num1, num2)
+		} else {
+			fmt.Println("Error: Division by zero.")
+			return
+		}
+	case "^":
+		result = Power() // Abujar TODO
+	default:
+		fmt.Println("Error: Invalid operator")
+		return
+	}
+
+	fmt.Printf("%.2f %s %.2f = %.2f\n", num1, operator, num2, result)
+}
